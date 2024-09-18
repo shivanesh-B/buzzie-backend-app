bcc579fdd4fe34cca0b3cb8650381f4bed8096ec
mdalmamunit427 <mdalmamunit427@gmail.com>
2024-09-19 01:25:46 +0600

    url update

--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -27,8 +27,8 @@ router.post("/create-checkout-session", async (req, res) => {
       line_items: lineItems,
       mode: "payment",
       success_url:
-        "http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "http://localhost:5173/cancel",
+        "https://lebaba-frontend-final.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "https://lebaba-frontend-final.vercel.app/cancel",
     });
 
     res.json({ id: session.id });
