b9035b3b516eeb5efa3dc38d608eb723d7813ed1
shivaneshB <shivaneshluckyboy@gmail.com>
2024-10-21 06:19:25 +0530

    url update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://buzzie-cart-frontend.vercel.app',
     credentials: true
 }))
 
